524aebc548eb9a96efa280839945256d7a706968
Dexter Ford <dexter.ford.77@gmail.com>
2017-01-12 11:01:50 -0600

    Adding moles

---
+++ whack.mole.js
@@ -0,0 +1,18 @@
+WHACK = WHACK || {}
+
+WHACK.Mole = (function() {
+
+  var Mole = function Mole () {
+    var _cheekyBastard = true,
+        _hungry = true,
+        _angry = "maybe",
+        _visible = false,
+        _screech = "AAARGH";
+
+    var emitScreech = function emitScreech () {
+      console.log(_screech);
+      return _screech;
+    }
+  }
+
+})();
